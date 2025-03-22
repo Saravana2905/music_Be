f989403e240965bb707034288617470064c96e7e
Saravanabalaji <101376863+Saravana2905@users.noreply.github.com>
2025-03-22 09:37:08 +0530

    Update UserModel.js

--- UserModel.js
+++ UserModel.js
@@ -3,6 +3,7 @@ const user = mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    role:{ type: String, required: true }
 })
 
 const User = mongoose.model('User', user);
